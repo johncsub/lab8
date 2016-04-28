71e1d6751d482f37d95a1f413cbfa53f2c6ee79b
Jose Reyes <jrbass00@gmail.com>
2016-04-28 16:24:36 -0700

    Moved basic menu code to personal files

--- joseR.cpp
+++ joseR.cpp
@@ -3,7 +3,10 @@
 // Program: joseR.cpp
 // Purpose: Draw and create a functioning menu that will allow
 //			the user to play, change settings, and exit the game.
+#include <GL/glx.h>
 #include "joseR.h"
+
+extern void init_opengl(); 
 
 void drawMenu(Game *game)
 {
@@ -18,11 +21,47 @@ void drawMenu(Game *game)
 	}
 }
 
-//void mouseOver(Shape *s)
-//{
-//	//if (game->mouseOnButton[i] == 1) {
-//	//	glColor3ub(190,90,190);
-//	//} else {
-//	//	glColor3ub(128,128,128);
-//	//}		
-//}
+void renderMenuObjects(Game *game)
+{
+	Shape *s;
+	init_opengl();
+	float w, h;
+	for (int i = 0; i < BUTTONS; i++) {
+		s = &game->box[i];
+		//Button colors based on mouse position
+		if (game->mouseOnButton[i] == 1) {
+			//Button selected color
+			glColor3ub(190,90,190);
+		} else {
+			//Button default color
+			glColor3ub(128,128,128);
+		}
+		glPushMatrix();
+		glTranslatef(s->center.x, s->center.y, s->center.z);
+		w = s->width;
+		h = s->height;
+		glBegin(GL_QUADS);
+		glVertex2i(-w,-h);
+		glVertex2i(-w, h);
+		glVertex2i( w, h);
+		glVertex2i( w,-h);
+		glEnd();
+		glPopMatrix();
+	}
+}
+
+void mouseOver(int savex, int savey, Game *game)
+{
+	Shape *s;
+	for (int j = 0; j < BUTTONS; j++) {			
+		s = &game->box[j];
+		if (savey >= s->center.y - (s->height) &&
+			savey <= s->center.y + (s->height) &&
+			savex >= s->center.x - (s->width) &&
+			savex <= s->center.x + (s->width)) {
+				game->mouseOnButton[j] = 1;
+		} else {
+			game->mouseOnButton[j] = 0;
+		}
+	}
+}
--- joseR.h
+++ joseR.h
@@ -5,7 +5,7 @@
 #define WINDOW_HEIGHT 768
 #define MAX_PARTICLES 2000
 #define GRAVITY 0.1
-//Number of buttons to show on menu
+//Number of buttons to show on menu, grows from bottom
 #define BUTTONS 3
 //X Position of all buttons
 //Divides WINDOW_WIDTH by the number below
@@ -41,5 +41,8 @@ struct Game {
 
 //Function Prototypes
 extern void drawMenu(Game *game);
+extern void mouseOver(int x, int y, Game *game);
+extern void renderMenuObjects(Game *game);
+
 
 #endif
