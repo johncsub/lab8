ad3fa6fc8a569ab6725be8f1d66163c8ebf6a64c
Jose Reyes <jrbass00@gmail.com>
2016-04-28 11:32:13 -0700

    First push

---
+++ joseR.cpp
@@ -0,0 +1,28 @@
+// Author:  Jose Reyes
+// Date:    April 28, 2016
+// Program: joseR.cpp
+// Purpose: Draw and create a functioning menu that will allow
+//			the user to play, change settings, and exit the game.
+#include "joseR.h"
+
+void drawMenu(Game *game)
+{
+	for (int j = 0; j < BUTTONS; j++) {		
+		game->buttonSpacer[j] = 650 - (j*90);
+		game->box[j].width = 120;
+		game->box[j].height = 25;
+		game->box[j].center.x = WINDOW_WIDTH / BUTTON_X;
+		game->box[j].center.y = WINDOW_HEIGHT - game->buttonSpacer[j];
+		//std::cout << game.box[j].center.y << std::endl;
+		//std::cout << game.box[j].center.x << std::endl;
+	}
+}
+
+//void mouseOver(Shape *s)
+//{
+//	//if (game->mouseOnButton[i] == 1) {
+//	//	glColor3ub(190,90,190);
+//	//} else {
+//	//	glColor3ub(128,128,128);
+//	//}		
+//}
