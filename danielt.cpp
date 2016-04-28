a39b84f6b9333bbb0dc9a00ab7558cafd388cf57
student <student@lab-315-018.cs.csubak.edu>
2016-04-28 09:55:46 -0700

    changes

--- danielt.cpp
+++ danielt.cpp
@@ -1,11 +1,89 @@
 //Daniel Turack
+//Program will control Enemy Missiles
+
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <X11/Xlib.h>
+//#include <X11/Xutil.h>
+//#include <GL/gl.h>
+//#include <GL/glu.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+//#include "log.h"
+//#include "ppm.h"
+//extern "C" {
+//	#include "fonts.h"
+//}
 using namespace std;
+
+//defined types
+typedef float Flt;
+typedef float Vec[3];
+typedef Flt Matrix[4][4];
+
+//macros
+#define rnd() (((double)rand())/(double)RAND_MAX)
+#define random(a) (rand()%a)
+#define VecZero(v) (v)[0]=0.0,(v)[1]=0.0,(v)[2]=0.0
+#define MakeVector(x, y, z, v) (v)[0]=(x),(v)[1]=(y),(v)[2]=(z)
+#define VecCopy(a,b) (b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2]
+#define VecDot(a,b)	((a)[0]*(b)[0]+(a)[1]*(b)[1]+(a)[2]*(b)[2])
+#define VecSub(a,b,c) (c)[0]=(a)[0]-(b)[0]; \
+			     (c)[1]=(a)[1]-(b)[1]; \
+(c)[2]=(a)[2]-(b)[2]
+//constants
+const float timeslice = 1.0f;
+const float gravity = -0.2f;
+#define PI 3.141592653589793
+#define ALPHA 1
+const int MAX_EMISSILES = 10;
+
+//X Windows variables
+//Display *dpy;
+//Window win;
+//GLXContext glc;
+
+
+//Setup timers
+const double physicsRate = 1.0 / 60.0;
+const double oobillion = 1.0 / 1e9;
+extern struct timespec timeStart, timeCurrent;
+extern struct timespec timePause;
+extern double physicsCountdown;
+extern double timeSpan;
+extern double timeDiff(struct timespec *start, struct timespec *end);
+extern void timeCopy(struct timespec *dest, struct timespec *source);
+//
+
+int xres=1250, yres=900;
+
+struct EMissile {
+    Vec pos;
+    Vec vel;
+    float color[3];
+    struct timespec time;
+    EMissile() { }
+};
+
+void eMissileCollision(void);
+void createEMissiles(void);
 
 void eMissileCollision()
 {
-  for (int i=1; i<4; i++) {
-    cout << "dturack test" << i << " " << endl;
-  }
+    for (int i=1; i<4; i++) {
+	cout << "dturack test" << i << " " << endl;
+    }
+}
+
+void createEMissiles()
+{
+    while (newMissiles) {
+
+    }
 }
