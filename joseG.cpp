ff689cf648b0c24183517748ee48d9662951faa8
jgarcia4 <jhazard18@yahoo.com>
2016-05-02 23:39:30 -0700

    changes 5/2/16

--- joseG.cpp
+++ joseG.cpp
@@ -1,36 +1,38 @@
-//Author: Jose Garcia 
+//Author: Jose Garcia
 //Program: joseG.cpp
-//Purpose: This program is my source code for the structures/cities
-//
-// #pragma once
+//Purpose: This program is my source code for rendering shapes that are supposed
+//to represent the floor and cities
+//Written: 4/28/16
+//Modified: 5/2/16
+
 #include "joseG.h"
 #include <GL/glx.h>
-extern void initOpengl();
+extern void init_opengl();
 void renderStruc(Structures *shape) {
-	initOpengl();
+	init_opengl();
         //floor shape
-        shape->floor.width = 512;
-        shape->floor.height = 100;
-        shape->floor.center.x = 512;
+        shape->floor.width = 400;
+        shape->floor.height = 50;
+        shape->floor.center.x = 400;
         shape->floor.center.y = 10;
 
-        /*//city shape
-        shape.city.width = 30;
-        shape.city.height = 50;
-        shape.city.center.x = 512;
-        shape.city.center.y = 65;
-*/
+        //city shape
+        shape->city.width = 30;
+        shape->city.height = 50;
+        shape->city.center.x = 400;
+        shape->city.center.y = 100;
 
-	float w, h/* w2, h2, w3, h3, w4, h4, w5, h5*/;
+	float w, h, w2, h2, w3, h3, w4, h4, w5, h5;
         //Draw shapes...
 
         //draw floor
         glColor3ub(255,255,255);
-        //s = shape->floor;
+	Shape *s;
+        s = &shape->floor;
         glPushMatrix();
-        glTranslatef(shape->floor.center.x, shape->floor.center.y, shape->floor.center.z);
-        w = shape->floor.width;
-        h = shape->floor.height;
+        glTranslatef(s->center.x, s->center.y, s->center.z);
+        w = s->width;
+        h = s->height;
         glBegin(GL_QUADS);
                 glVertex2i(-w,-h);
                 glVertex2i(-w, h);
@@ -39,14 +41,16 @@ void renderStruc(Structures *shape) {
         glEnd();
         glPopMatrix();
 
-        /*//draw cities
+        //draw cities
+	//I might try to render cities in a loop
+	//Not sure if I can
         Shape *s2;
         glColor3ub(87,87,87);
-        s2 = shape.city;
+        s2 = &shape->city;
         glPushMatrix();
-        glTranslatef(s2.center.x - 200, s2.center.y, s2.center.z);
-        w2 = s2.width;
-        h2 = s2.height;
+        glTranslatef(s2->center.x - 100, s2->center.y, s2->center.z);
+        w2 = s2->width;
+        h2 = s2->height;
         glBegin(GL_QUADS);
                 glVertex2i(-w2,-h2);
                 glVertex2i(-w2, h2);
@@ -57,9 +61,9 @@ void renderStruc(Structures *shape) {
 
         Shape *s3;
         //glColor3ub(87,87,87);
-        s3 = shape.city;
+        s3 = &shape->city;
         glPushMatrix();
-        glTranslatef(s3->center.x - 400, s3->center.y, s3->center.z);
+        glTranslatef(s3->center.x - 300, s3->center.y, s3->center.z);
         w3 = s3->width;
         h3 = s3->height;
         glBegin(GL_QUADS);
@@ -72,9 +76,9 @@ void renderStruc(Structures *shape) {
 
         Shape *s4;
         //glColor3ub(87,87,87);
-        s4 = shapes.city;
+        s4 = &shape->city;
         glPushMatrix();
-        glTranslatef(s4->center.x + 200, s4->center.y, s2->center.z);
+        glTranslatef(s4->center.x + 100, s4->center.y, s2->center.z);
         w4 = s4->width;
         h4 = s4->height;
         glBegin(GL_QUADS);
@@ -87,9 +91,9 @@ void renderStruc(Structures *shape) {
 
         Shape *s5;
         //glColor3ub(87,87,87);
-        s5 = &shapes->city;
+        s5 = &shape->city;
         glPushMatrix();
-        glTranslatef(s5->center.x + 400, s5->center.y, s5->center.z);
+        glTranslatef(s5->center.x + 300, s5->center.y, s5->center.z);
         w5 = s5->width;
         h5 = s5->height;
         glBegin(GL_QUADS);
@@ -98,6 +102,6 @@ void renderStruc(Structures *shape) {
                 glVertex2i( w5, h5);
                 glVertex2i( w5,-h5);
         glEnd();
-        glPopMatrix();*/
+        glPopMatrix();
 
 }
